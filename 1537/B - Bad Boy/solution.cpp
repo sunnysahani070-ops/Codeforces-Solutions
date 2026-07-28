4d47c5a5195263dda49b402ef9d2bf6ae4dd68a0
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-07-28 22:46:49 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+void solve() {
+    long long n, m, i, j;
+    cin >> n >> m >> i >> j;
+    
+    
+    cout << 1 << " " << 1 << " " << n << " " << m << "
+";
+}
+ 
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
