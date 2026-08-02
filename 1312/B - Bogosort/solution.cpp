c6b7624261232a030f792f73d6dc2252600dd072
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-02 15:46:11 +0530

    Add Bogosort [B] from Codeforces

---
+++ 1312/B - Bogosort/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    
+    
+    sort(a.rbegin(), a.rend());
+    
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << (i == n - 1 ? "" : " ");
+    }
+    cout << "
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
