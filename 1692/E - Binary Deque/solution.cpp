be71efe69712a5b7a8a0c450138f12ba0c968a7f
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-10 22:06:57 +0530

    Add Binary Deque [E] from Codeforces

---
+++ 1692/E - Binary Deque/solution.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+using namespace std;
+ 
+void solve() {
+    int n, s;
+    cin >> n >> s;
+    vector<int> a(n);
+    int total_sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        total_sum += a[i];
+    }
+    
+    if (total_sum < s) {
+        cout << -1 << endl;
+        return;
+    }
+    
+    if (total_sum == s) {
+        cout << 0 << endl;
+        return;
+    }
+ 
+    int left = 0;
+    int current_sum = 0;
+    int max_len = 0;
+ 
+   
+    for (int right = 0; right < n; right++) {
+        current_sum += a[right];
+        
+        while (current_sum > s) {
+            current_sum -= a[left];
+            left++;
+        }
+        
+        if (current_sum == s) {
+            max_len = max(max_len, right - left + 1);
+        }
+    }
+ 
+    cout << n - max_len << endl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
