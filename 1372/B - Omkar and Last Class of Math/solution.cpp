62c5eb171c34af596bb0d51268072731d12df444
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-15 11:47:25 +0530

    Add Omkar and Last Class of Math [B] from Codeforces

---
+++ 1372/B - Omkar and Last Class of Math/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    
+    for (long long i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            
+            int a = n / i; 
+            int b = n - a;
+            cout << a << " " << b << endl;
+            return;
+        }
+    }
+    
+    cout << 1 << " " << n - 1 << endl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
