080af1d6f0fd0d64261e8978d8e17e7298d1e886
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-23 17:00:37 +0530

    Add Greetings [F] from Codeforces

---
+++ 1915/F - Greetings/solution.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i].first >> a[i].second;
+    }
+    
+    sort(a.begin(), a.end());
+    
+    ordered_set os;
+    long long ans = 0;
+    
+    for (int i = n - 1; i >= 0; --i) {
+        ans += os.order_of_key(a[i].second);
+        os.insert(a[i].second);
+    }
+    
+    cout << ans << "
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
